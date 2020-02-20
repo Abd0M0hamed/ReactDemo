1f7dfa734a3836b95cd71984db8cf76dce33f0f2
Abdo <Abdo.Tasks@gmail.com>
2020-02-20 12:49:33 +0200

    Refactored TopBar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,9 +1,37 @@
 import React from "react";
+import TopBarItem from "./TopBarItem"
 
 const TopBar = () => {
+  const barElements = [
+    {title: "Home", href: "#home"},
+    {title: "Projects", href: "#projects"},
+    {title: "Contact Us", href: "#contact-us"},
+    {title: "About Us", href: "#about-us"},
+  ]
   return (
       <div className="top-bar">
-        <span>LOGO</span> &nbsp;&nbsp;&nbsp; Home : Projects : Contact Us : About Us
+        <span>LOGO</span> &nbsp;&nbsp;&nbsp;
+
+          <TopBarItem
+            title="Home"
+            href="#home"
+          />&nbsp;:&nbsp;
+
+          <TopBarItem
+            title="Projects"
+            href="#projects"
+          />&nbsp;:&nbsp;
+
+          <TopBarItem
+            title="Contact Us"
+            href="#contact-us"
+          />&nbsp;:&nbsp;
+
+          <TopBarItem
+            title="About Us"
+            href="#about-us"
+          />
+
       </div>
   )
 }
