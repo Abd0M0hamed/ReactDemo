0ade2730955603b2eb4cc6edc3ffeaee8890386d
Abdo <Abdo.Tasks@gmail.com>
2020-02-20 12:52:08 +0200

    Beautify HTML code

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,11 +1,13 @@
 import React from "react"
 
 const CardItem = (props) => {
-  return (<div className="card">
-    <img src={props.contact.imageUrl} />
-    <h3>{props.contact.cardTitle}</h3>
-    <div>{props.contact.cardDescription}</div>
-  </div>)
+  return (
+    <div className="card">
+      <img src={props.contact.imageUrl} />
+      <h3>{props.contact.cardTitle}</h3>
+      <div>{props.contact.cardDescription}</div>
+    </div>
+  )
 }
 
 export default CardItem
