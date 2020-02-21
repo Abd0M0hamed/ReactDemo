d77d35056bc9c02c48ae2128a8bd8e9af95bff20
Abdo <Abdo.Tasks@gmail.com>
2020-02-21 15:58:39 +0200

    Move JokesData To Separate file

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,43 +1,27 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import JokeItem from "./JokeItem"
+import JokesData from "./JokesData"
 
 const Jokes = () => {
-  const JokesData = [
-    {question: "Question 1?", answer: "Answer 1."},
-    {question: "Question 2?", answer: "Answer 2."},
-    {question: "Question 3?", answer: "Answer 3."},
-    {question: "Question 4?", answer: "Answer 4."},
-    {question: "Question 5?", answer: "Answer 5."},
-    {question: "Question 6?", answer: "Answer 6."},
-    {question: "Question 7?", answer: "Answer 7."},
-    {question: "Question 8?", answer: "Answer 8."},
-    {question: "Question 9?", answer: "Answer 9."},
-    {question: "Question 10?", answer: "Answer 10."}
-  ];
+
+  const JokesList = JokesData.map(item => <JokeItem key={item.id} question={item.question} answer={item.answer} />)
+
   return(
     <div>
-    
+
       <JokeItem
         question=""
         answer="Answer 1."
       />
+
       <JokeItem
         question="Question 2?"
         answer=""
       />
-      <JokeItem
-        question="Question 3?"
-        answer="Answer 3."
-      />
-      <JokeItem
-        question="Question 4?"
-        answer="Answer 4."
-      />
-      <JokeItem
-        question="Question 5?"
-        answer="Answer 5."
-      />
+
+      {JokesList}
+
     </div>
   )
 }
