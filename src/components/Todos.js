35f8d95e3e1f0fbd608a217bce82f85ed670f53b
Abdo <Abdo.Tasks@gmail.com>
2020-02-23 00:58:59 +0200

    Modify State & Handle Events

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -7,27 +7,56 @@ class Todos extends React.Component {
   constructor(){
     super()
     this.state = {
-      allDone: "No"
+      allDone: "No",
+      counter: 0
     }
+    this.setAllDone = this.setAllDone.bind(this)
+    this.setAllNotDone = this.setAllNotDone.bind(this)
   }
-  
+
   getTodoList(){
     return TodosData.map(item => <TodoItem key={item.id} data={item} />)
+  }
+
+  setAllDone(){
+    this.setState({ allDone: "Yes" })
+    // OR
+    /*this.setState(prevState => {
+      return "Hii"
+    })*/
+  }
+
+  setAllNotDone() {
+    this.setState({ allDone: "No" })
+  }
+
+  incrementCounter(){
+    this.setState(prevState => {
+      return {
+        counter: prevState.counter + 1
+      }
+    })
   }
 
   render(){
     const TodoList = this.getTodoList();
     return (
       <div className="todo-list">
-        <div>All Done? {this.state.allDone}</div>
-        {TodoList}
+        <div>All Done? <span style={{color: "red", fontWeight: "bold"}}>{this.state.allDone}</span></div>
         <div>
-          <button>Mall All Done.</button>
+          <button onClick={this.setAllDone}>Make All Done.</button>
+          <button onClick={this.setAllNotDone}>Make All Not Done.</button>
         </div>
+        <div>
+          <p>{this.state.counter}</p>
+          <button onClick={this.incrementCounter}>Increment.</button>
+        </div>
+        {TodoList}
         <hr />
       </div>
     );
   }
+
 }
 
 export default Todos
