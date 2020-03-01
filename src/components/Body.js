d32238c5051e7edfc246a6c5f3fb0f6301430d65
Abdo <Abdo.Tasks@gmail.com>
2020-03-01 17:37:32 +0200

    Created ContactUs Form

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,13 +4,16 @@ import Cards from "./Cards"
 import Jokes from "./Jokes"
 import Products from "./Products"
 import RemoteCaller from "./RemoteCaller"
+import ContactUs from "./ContactUs"
 
 class Body extends React.Component {
 
   render(){
-    
+
     return (
       <div id="body">
+        <br />
+        <ContactUs />
         <br />
         <RemoteCaller />
         <br />
