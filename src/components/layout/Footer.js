fda8d9f919ec3635620317b271317537fa7560af
Abdo Mohamed <Abdo.Tasks@gmail.com>
2020-08-07 00:23:21 +0200

    Fix Signature

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,7 +4,7 @@ class Footer extends React.Component {
 
   render(){
     return (
-      <div id="footer">CopyRight &copy; 2020 By AbdoMohamed&trade; </div>
+      <div id="footer">CopyRight &copy; 2020 By AbdelrahmanHelaly&trade; </div>
     )
   }
 
